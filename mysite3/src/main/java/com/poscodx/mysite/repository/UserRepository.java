8f6baab17d9299295d44e8c69566ac8279d35079
parlyresk <parlyresk325@gmail.com>
2024-06-03 14:49:44 +0900

    mysite3 user

---
+++ mysite3/src/main/java/com/poscodx/mysite/repository/UserRepository.java
@@ -0,0 +1,152 @@
+package com.poscodx.mysite.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.stereotype.Repository;
+
+import com.poscodx.mysite.vo.UserVo;
+
+@Repository
+public class UserRepository {
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.0.212:3306/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		}
+
+		return conn;
+	}
+
+	public void insert(UserVo vo) {
+		try (Connection conn = getConnection();
+
+			PreparedStatement pstmt1 = conn.prepareStatement("insert into user values(null,?,?,password(?),?,current_date())");
+				) {
+
+			// 4. binding
+			pstmt1.setString(1, vo.getName());
+			pstmt1.setString(2, vo.getEmail());
+			pstmt1.setString(3, vo.getPassword());
+			pstmt1.setString(4, vo.getGender());
+
+			// 5. SQL 실행
+			pstmt1.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+		
+	}
+
+	public UserVo findByEmailAndPassword(String email, String password) {
+		UserVo result=null;
+		
+		try (Connection conn = getConnection();
+
+				PreparedStatement pstmt = conn.prepareStatement("select no,name from user where email=? and password=password(?)");
+					) {
+
+				// 4. binding
+				pstmt.setString(1, email);
+				
+				pstmt.setString(2, password);
+				
+
+				// 5. SQL 실행
+				ResultSet rs = pstmt.executeQuery();
+				if(rs.next()) {
+					Long no = rs.getLong(1);
+					String name = rs.getString(2);
+					result=new UserVo();
+					result.setNo(no);
+					result.setName(name);
+				}
+				rs.close();
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		
+		return result;
+	}
+
+	public UserVo findByNo(Long no) {
+		UserVo result=null;
+		
+		try (Connection conn = getConnection();
+
+				PreparedStatement pstmt = conn.prepareStatement("select no,name,email,gender from user where no=?");
+					) {
+
+				// 4. binding
+				pstmt.setLong(1, no);
+				
+				
+				
+
+				// 5. SQL 실행
+				ResultSet rs = pstmt.executeQuery();
+				if(rs.next()) {
+					Long no_2 = rs.getLong(1);
+					String name = rs.getString(2);
+					String email = rs.getString(3);
+					String gender = rs.getString(4);
+					result=new UserVo();
+					result.setNo(no_2);
+					result.setName(name);
+					result.setEmail(email);
+					result.setGender(gender);
+				}
+				rs.close();
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		
+		return result;
+	}
+
+	public void update(UserVo vo) {
+		int count=0;
+		try (Connection conn = getConnection();
+
+				PreparedStatement pstmt1 = conn.prepareStatement("UPDATE user SET name=?, password=password(?), gender=? WHERE no=?");
+				PreparedStatement pstmt2 = conn.prepareStatement("UPDATE user SET name=?, gender=? WHERE no=?");
+					) {
+				
+				if("".equals(vo.getPassword())) {
+					pstmt2.setString(1, vo.getName());
+					pstmt2.setString(2, vo.getGender());
+					pstmt2.setLong(3, vo.getNo());
+					count = pstmt2.executeUpdate();
+				}else {
+					pstmt1.setString(1, vo.getName());
+					pstmt1.setString(2, vo.getPassword());
+					pstmt1.setString(3, vo.getGender());
+					pstmt1.setLong(4, vo.getNo());
+					
+					count = pstmt1.executeUpdate();
+				}
+				// 4. binding
+				
+				
+				
+				
+
+				
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		
+		
+	}
+	
+	
+}
---
+++ mysite3/src/main/webapp/WEB-INF/views/board/list.jsp
@@ -0,0 +1,101 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>mysite</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<link
+	href="${pageContext.servletContext.contextPath }/assets/css/board.css"
+	rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div id="container">
+		<c:import url="/WEB-INF/views/includes/header.jsp" />
+		<div id="content">
+			<div id="board">
+				<form id="search"
+					action="${pageContext.request.contextPath}/board?page=1" method="post">
+					<input type="text" id="kwd" name="kwd" value="">
+					
+					<input
+						type="submit" value="찾기">
+				</form>
+				<table class="tbl-ex">
+					<tr>
+						<th>번호</th>
+						<th>제목</th>
+						<th>글쓴이</th>
+						<th>조회수</th>
+						<th>작성일</th>
+						<th>&nbsp;</th>
+					</tr>
+					<c:set var="count" value="${fn:length(list) }" />
+					<c:forEach var="boardVo" items="${list}" varStatus="status">
+						<tr>
+							<td>${totalPosts-status.index-(currentPage-1)*postsPerPage}</td>
+							<td style="text-align:left; padding-left:${20 * boardVo.depth}px">
+								<c:if test="${boardVo.depth > 0}">
+									<img
+										src='${pageContext.servletContext.contextPath}/assets/images/reply.png'>
+								</c:if> <a
+								href="${pageContext.request.contextPath}/board?a=view&no=${boardVo.no}&page=${currentPage}&kwd=${kwd}">${boardVo.title}</a>
+							</td>
+							<td>${boardVo.userName}</td>
+							<td>${boardVo.hit}</td>
+							<td>${boardVo.regDate}</td>
+							<td><c:if
+									test="${sessionScope.authUser.no eq boardVo.userNo}">
+									<a
+										href="${pageContext.request.contextPath}/board?a=delete&no=${boardVo.no}&page=${currentPage}&kwd=${kwd}"
+										class="del">삭제</a>
+								</c:if></td>
+						</tr>
+					</c:forEach>
+				</table>
+
+				<!-- pager 추가 -->
+				<div class="pager">
+					<ul>
+						<c:if test="${currentPage > 1}">
+							<li><a href="?page=${currentPage - 1}&kwd=${kwd}">◀</a></li>
+						</c:if>
+						
+						<c:forEach var="i" begin="${currentNavStart}" end="${currentNavEnd}">
+							<c:choose>
+								<c:when test="${i <= totalPages}">
+									<li class="${i == currentPage ? 'selected' : ''}"><a
+										href="?page=${i}&kwd=${kwd}">${i}</a></li>
+								</c:when>
+								<c:otherwise>
+									<li>${i}</li>
+								</c:otherwise>
+							</c:choose>
+						</c:forEach>
+						<c:if test="${currentPage < totalPages}">
+							<li><a href="?page=${currentPage + 1}&kwd=${kwd}">▶</a></li>
+						</c:if>
+
+					</ul>
+				</div>
+				<!-- pager 추가 -->
+
+				<div class="bottom">
+					<c:if test="${not empty sessionScope.authUser}">
+						<a
+							href="${pageContext.request.contextPath}/board?a=writeform&page=${currentPage}&kwd=${kwd}"
+							id="new-book">글쓰기</a>
+					</c:if>
+				</div>
+			</div>
+		</div>
+		<c:import url="/WEB-INF/views/includes/navigation.jsp">
+			<c:param name="menu" value="board" />
+		</c:import>
+		<c:import url="/WEB-INF/views/includes/footer.jsp" />
+	</div>
+</body>
+</html>
