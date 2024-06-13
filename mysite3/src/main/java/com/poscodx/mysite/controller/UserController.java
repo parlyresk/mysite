5628503debaa5e0931a2a723976f959dea755f92
parlyresk <parlyresk325@gmail.com>
2024-06-13 13:04:29 +0900

    validation

--- mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
 	private UserService userService;
 	
 	@RequestMapping(value="/join",method=RequestMethod.GET)
-	public String join() {
+	public String join(@ModelAttribute UserVo vo) {
 		return "user/join";
 		
 	}
@@ -36,7 +36,7 @@ public class UserController {
 			return "user/join";
 		}
 		
-		// userService.join(vo);
+		userService.join(vo);
 		
 		
 		return "redirect:/user/joinsuccess";
