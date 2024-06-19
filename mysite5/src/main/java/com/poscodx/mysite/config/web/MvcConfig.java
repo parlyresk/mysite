389eefd540840124084bba25383dcc697d5fc52f
parlyresk <parlyresk325@gmail.com>
2024-06-19 13:52:00 +0900

    web xml 대체함

--- mysite5/src/main/java/com/poscodx/mysite/config/web/MvcConfig.java
+++ mysite5/src/main/java/com/poscodx/mysite/config/web/MvcConfig.java
@@ -13,8 +13,8 @@ import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -71,11 +71,19 @@ public class MvcConfig implements WebMvcConfigurer{
 		converters.add(stringHttpMessageConverter());
 		converters.add(mappingJackson2HttpMessageConverter());
 	}
-
-	// Default Servlet Handler
+	
+	//static (assets) url mapping
 	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
-		
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+		.addResourceHandler("/assets/**")
+		.addResourceLocations("classpath:assets/");
 	}
+
+//	// Default Servlet Handler
+//	@Override
+//	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//		configurer.enable();
+//		
+//	}
 }
