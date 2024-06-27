075d099df19bde87d45d698532d4c3eb06668a7f
parlyresk <parlyresk325@gmail.com>
2024-06-27 11:40:14 +0900

    mysite7 #1

---
+++ mysite7/src/main/java/com/poscodx/mysite/config/MvcConfig.java
@@ -0,0 +1,89 @@
+package com.poscodx.mysite.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import com.poscodx.mysite.event.ApplicationContextEventListener;
+import com.poscodx.mysite.interceptor.SiteInterceptor;
+
+@SpringBootConfiguration
+public class MvcConfig implements WebMvcConfigurer {
+	@Autowired
+	private Environment env;
+
+	// Locale Resolver
+	@Bean
+	public LocaleResolver localeResolver() {
+		CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+		localeResolver.setCookieName("lang");
+		localeResolver.setCookieHttpOnly(false);
+
+		return localeResolver;
+	}
+
+	// View Resolver
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setExposeContextBeansAsAttributes(true);
+		viewResolver.setExposedContextBeanNames("site");
+		viewResolver.setViewNames("views/*");
+		viewResolver.setOrder(1);
+
+		return viewResolver;
+	}
+
+	// Thymeleaf View Resolver
+	@Bean
+	public ViewResolver thymeleafViewResolver(ISpringTemplateEngine templateEngine) {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+
+		viewResolver.setTemplateEngine(templateEngine);
+		viewResolver.setCharacterEncoding("UTF-8");
+		viewResolver.setOrder(1);
+
+		return viewResolver;
+	}
+
+	// Site Interceptor
+	@Bean
+	public HandlerInterceptor siteInterceptor() {
+		return new SiteInterceptor();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(siteInterceptor()).addPathPatterns("/**").excludePathPatterns("/assets/**");
+	}
+
+	// ApplicationContextEventListener
+	@Bean
+	public ApplicationContextEventListener applicationContextEventListener() {
+		return new ApplicationContextEventListener();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler(env.getProperty("fileupload.resourceUrl") + "/**")
+				.addResourceLocations("file:" + env.getProperty("fileupload.uploadLocation") + "/");
+
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:assets/");
+	}
+
+}
